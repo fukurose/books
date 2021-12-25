5e042fd121ec5c30d2e565b806f98ef8056b43d5
fukurose <xylitoall@gmail.com>
2021-12-25 14:31:04 +0900

    use socket

--- RealWorkdHTTP/client/chunk/main.go
+++ RealWorkdHTTP/client/chunk/main.go
@@ -2,24 +2,56 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"io"
 	"log"
+	"net"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
-	resp, err := http.Get("http://localhost:18888/chunked")
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	conn, err := dialer.Dial("tcp", "localhost:18888")
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
-	reader := bufio.NewReader(resp.Body)
+	defer conn.Close()
+
+	request, err := http.NewRequest("GET", "http://localhost:18888/chunked", nil)
+	if err != nil {
+		panic(err)
+	}
+	err = request.Write(conn)
+	if err != nil {
+		panic(err)
+	}
+	reader := bufio.NewReader(conn)
+	resp, err := http.ReadResponse(reader, request)
+	if err != nil {
+		panic(err)
+	}
+	if resp.TransferEncoding[0] != "chunked" {
+		panic("wrong transfer encoding")
+	}
 	for {
-		line, err := reader.ReadBytes('\n')
+		sizeStr, err := reader.ReadBytes('\n')
 		if err == io.EOF {
 			break
 		}
-		log.Println(string(bytes.TrimSpace(line)))
+		size, err := strconv.ParseInt(string(sizeStr[:len(sizeStr)-2]), 16, 64)
+		if size == 0 {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		line := make([]byte, int(size))
+		reader.Read(line)
+		reader.Discard(2)
+		log.Print(" ", string(line))
 	}
 }
